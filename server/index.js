056d3214aae8c5583065cb06ec45096cd9f7f61e
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-12-14 17:58:57 +0700

    doneee

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8080
+
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send({
+    "courses":[
+      "Python",
+      "Java",
+      "PHP",
+      "JS",
+
+    ]
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
